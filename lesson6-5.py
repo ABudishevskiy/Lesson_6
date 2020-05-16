d3595e3d951daaa75779671a5f04d8c9e9274862
ABudishevskiy <56083073+ABudishevskiy@users.noreply.github.com>
2020-05-16 12:22:19 +0300

    Add files via upload

--- lesson6-5.py
+++ lesson6-5.py
@@ -3,20 +3,28 @@
 # [[], [1, 2], [2, 3, [1, 2]], [1, 2, 3]]
 
 lis = [[], [1, 2], [2, 3, [1, [9, 6], 2, [7, 8, [4, 5]]]], [1, 2, 3]]
-def rev(a):
+# def rev(a):
+#     newlist = []
+#     for i in a:
+#         if isinstance(i, list):
+#             count = 0
+#             for k in i:
+#                 if isinstance(k, list):
+#                     for l in rev(i)[count: ]:
+#                         newlist.append(l)
+#                 else:
+#                     count = count + 1
+#                     newlist.append(k)
+#         else:
+#             newlist.append(i)
+#     return newlist
+def rev2(a):
     newlist = []
     for i in a:
         if isinstance(i, list):
-            count = 0
-            for k in i:
-                if isinstance(k, list):
-                    for l in rev(i)[count: ]:
-                        newlist.append(l)
-                else:
-                    count = count + 1
-                    newlist.append(k)
+            newlist.extend(rev2(i))
         else:
             newlist.append(i)
     return newlist
 
-print(rev(lis))
+print(rev2(lis))
